b0543ba2fa93dc5077c17581205a1276c9bd1a5c
Andrialow <andre.marques4040@gmail.com>
2016-12-06 15:19:15 +0000

    Update Contestants.java

--- Contestants.java
+++ Contestants.java
@@ -1,4 +1,5 @@
 
+
 public class Contestants {
    private static final int MAX_CONTESTANTS = 10;
    
@@ -12,7 +13,18 @@ public class Contestants {
 	   currentContestant = 0;
 	   
    }
-
+   private int indexOf(String name){
+	   int i = 0;
+	   int result = -1;
+	   
+	   while((i < counter) && (result == -1)){
+		   if(bestScorers[i].getName().equals(name)){
+			   result = i;
+			  i++; 
+		   }
+	   }
+	   return result;
+   }
    private void resize(){
 	   BestScorer[] tmp = new BestScorer[2*bestScorers.length];
 	   int i = 0;
@@ -24,5 +36,46 @@ public class Contestants {
    public void initializeIterator(){
 	   currentContestant = 0;
    }
+   public boolean hasNext(){
+		 return ((currentContestant >= 0) && 
+				 (currentContestant < counter));
+	  }
+	  public BestScorer next(){
+		  return bestScorers[currentContestant++];
+	  }
+	public int getNumberOfContestants(){
+		return counter;
+	}
+	  private int findPositionFor(BestScorer b){
+		  int contestantPosition = 0;
+		  boolean found = false;
+		  while(contestantPosition < counter && !found){
+			  if(b.compareTo(bestScorers[contestantPosition])>0){
+				 contestantPosition++;}
+				 else 
+					 found = true;
+			  }
+			  return contestantPosition;
+		  }
+	  
+	  public void addBestScorer(String name, int award){
+		  if(counter == bestScorers.length)
+			  resize();
+		  BestScorer b = new BestScorer(name, award);
+		  int position = findPositionFor(b);
+		  addBestScorerAt(b, position);
+	  }
+	  private void addBestScorerAt(BestScorer c, int position){
+		    counter++;
+		    for(int i = counter - 1; i>=position; i--)
+		    	bestScorers[i+1] = bestScorers [i];
+		    
+		    bestScorers[position] = c;
+		    counter++;
 
+	  }  
+	  public boolean hasContestant(String name){
+		  return indexOf(name) >=0;
+	  }
+	 
 }
